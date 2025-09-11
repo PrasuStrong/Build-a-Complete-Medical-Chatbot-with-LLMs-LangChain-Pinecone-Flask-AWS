7fdfd19df62571faefef9fd542d24af8561f546f
Prasuna <prasumarepally@gmail.com>
2025-09-11 01:14:29 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Prasuna/Bappy",
+    author_email="prasumarepally@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
